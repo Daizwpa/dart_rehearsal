58faea0870c412b765bd7adaf6c4611e6d91217d
Daiz Wap <zmatder@outlook.com>
2024-08-01 23:03:01 +0100

    first

---
+++ controlFlow.statements.dart
@@ -0,0 +1,12 @@
+void main() {
+  if (5 > 2) {
+    if (10 > 2) {
+      if (10 < 2) {
+      } else {
+        print("ok");
+      }
+    } else if (10 < 11) {
+      print("ok2");
+    }
+  }
+}
---
+++ variables.dart
@@ -0,0 +1,8 @@
+void main() {
+  var firstName = "dax";
+  var lastName = "wap";
+  var age = 23;
+  var weight = 65.3;
+  var numbers = [12, 33, 31, 90, 0];
+  var data = {"Tags": "<good>", "url": "www.google.com"};
+}
